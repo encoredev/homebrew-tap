30563e83893dc537bdf8c7538d6978bd1ce196db
André Eriksson <andre@encore.dev>
2021-07-08 12:50:57 +0200

    Release v0.16.2

--- Formula/encore.rb
+++ Formula/encore.rb
@@ -1,17 +1,17 @@
 class Encore < Formula
   desc "The static analysis-powered Go framework for building backend applications"
   homepage "https://encore.dev"
-  version "0.16.1"
+  version "0.16.2"
   license ""
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://d2f391esomvqpi.cloudfront.net/encore-0.16.1-darwin_amd64.tar.gz"
-    sha256 "773f6787dff49d8f9fcea8b3afdc45ec2788798f9de1be5f87a7c73604b4f57a"
+    url "https://d2f391esomvqpi.cloudfront.net/encore-0.16.2-darwin_amd64.tar.gz"
+    sha256 "8684c0dc150c5b7eca3c45e223db5b2ed7862db7c32d25f50a47770a21cff0e1"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://d2f391esomvqpi.cloudfront.net/encore-0.16.1-darwin_arm64.tar.gz"
-    sha256 "f2d3e8612fba68ae874390441aaff4882f2fff4f62188135d63eaa98cc5b39cd"
+    url "https://d2f391esomvqpi.cloudfront.net/encore-0.16.2-darwin_arm64.tar.gz"
+    sha256 "dfdb0f5a0e644147837b427265528c941e1e46dabc15808a0c4540cf4d25caab"
   end
 
   def install
