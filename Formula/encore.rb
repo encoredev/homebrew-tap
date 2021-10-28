277419005ee67ac800d2da6a4d893617c5d3805c
André Eriksson <andre@encore.dev>
2021-10-28 19:57:22 +0200

    Release fixed v0.17.3

--- Formula/encore.rb
+++ Formula/encore.rb
@@ -6,11 +6,11 @@ class Encore < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://d2f391esomvqpi.cloudfront.net/encore-0.17.3-darwin_amd64.tar.gz"
-    sha256 "c97c874a22439f008013b227181bb90e61afd6d0ee93d50fe34f2e08ca894b36"
+    sha256 "d07a91159c475315ac28d9e182e8ff8e6d50788845bf1f6710adff67a9aa0def"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://d2f391esomvqpi.cloudfront.net/encore-0.17.3-darwin_arm64.tar.gz"
-    sha256 "51ac4ea1a77fc9cd4239539ceb04ca51eebcd3623ad517d00f57100dd65a33c4"
+    sha256 "f56e0d3422fd7e58ede8a482a27584cbb5a5f5522ad5c119eb512339a9f42f0c"
   end
 
   def install
