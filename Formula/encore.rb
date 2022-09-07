dda9f42b5972d888e0688a341623f8aa5319804d
André Eriksson <andre@encore.dev>
2022-09-07 18:25:23 +0200

    Release v1.6.0

--- Formula/encore.rb
+++ Formula/encore.rb
@@ -1,17 +1,25 @@
 class Encore < Formula
   desc "The static analysis-powered Go framework for building backend applications"
   homepage "https://encore.dev"
-  version "1.5.1"
+  version "1.6.0"
   license "Mozilla Public License, version 2.0"
   head "https://github.com/encoredev/encore.git", branch: "main"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://d2f391esomvqpi.cloudfront.net/encore-1.5.1-darwin_amd64.tar.gz"
-    sha256 "59768cf04646dc6a2f91857a3e62da036452123a4d81166bc3aeeb9108b3fb3e"
+    url "https://d2f391esomvqpi.cloudfront.net/encore-1.6.0-darwin_amd64.tar.gz"
+    sha256 "28339f71d8f9ce7517a84bad7a0d9989fe3d67fce88e564f2b40bf5d5ffa5d3e"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://d2f391esomvqpi.cloudfront.net/encore-1.5.1-darwin_arm64.tar.gz"
-    sha256 "38e9cca0d3268791f76843882d36c4a19a826eec9e3a5a4f51cf824aad9d0336"
+    url "https://d2f391esomvqpi.cloudfront.net/encore-1.6.0-darwin_arm64.tar.gz"
+    sha256 "8d915f9e383bd05a28615273baace7a1f2026fe090b071ab31575cdd854179e1"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://d2f391esomvqpi.cloudfront.net/encore-1.6.0-linux_amd64.tar.gz"
+    sha256 "5fdeca69adf20228f687e3de558be13200b26068d1b463a1e11fbdedad9ffc53"
+  end
+  if OS.linux? && Hardware::CPU.arm?
+    url "https://d2f391esomvqpi.cloudfront.net/encore-1.6.0-linux_arm64.tar.gz"
+    sha256 "4d1403eec337f6cf86daf12db9d36904a14564b68c5e54a5a55a69d9d9af77da"
   end
 
   def install
