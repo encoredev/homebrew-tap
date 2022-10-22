78b247cac1750f603faf86d02bc635b7ec1cbe01
Dominic Black <dom@encore.dev>
2022-10-22 16:17:07 +0100

    Update encore@nightly.rb

--- Formula/encore@nightly.rb
+++ Formula/encore@nightly.rb
@@ -1,4 +1,4 @@
-class EncoreAtNighly < Formula
+class EncoreATNighly < Formula
     desc "The static analysis-powered Go framework for building backend applications"
     homepage "https://encore.dev"
     license "Mozilla Public License, version 2.0"
