77f661f4626f5211dfaf95237f6706dec5ef8487
Dominic Black <me@jdm.black>
2022-10-25 19:22:32 +0000

    Release Encore v1.9.2
    This commit bumps the Encore version to v1.9.2
    
    _This PR was created by the Encore release process automatically._

--- Formula/encore.rb
+++ Formula/encore.rb
@@ -1,45 +1,48 @@
 class Encore < Formula
-  desc "The static analysis-powered Go framework for building backend applications"
-  homepage "https://encore.dev"
-  version "1.9.1"
-  license "Mozilla Public License, version 2.0"
-  head "https://github.com/encoredev/encore.git", branch: "main"
+    desc "The static analysis-powered Go framework for building backend applications"
+    homepage "https://encore.dev"
+    license "Mozilla Public License, version 2.0"
+    head "https://github.com/encoredev/encore.git", branch: "main"
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://d2f391esomvqpi.cloudfront.net/encore-1.9.1-darwin_amd64.tar.gz"
-    sha256 "9edd36838e0c08ee8a93c61238439a7bad521f6d7e8be03a81d83fac1c54215c"
-  end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://d2f391esomvqpi.cloudfront.net/encore-1.9.1-darwin_arm64.tar.gz"
-    sha256 "cc4405e56b768f1bee8ab5a92779754fde4148d89bbc31cb40303a4a5aa93b4e"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://d2f391esomvqpi.cloudfront.net/encore-1.9.1-linux_amd64.tar.gz"
-    sha256 "df161f28e7ea07285c7de48d35fde58de64464f1e03b07d1fadc0ac41c6ca52a"
-  end
-  if OS.linux? && Hardware::CPU.arm?
-    url "https://d2f391esomvqpi.cloudfront.net/encore-1.9.1-linux_arm64.tar.gz"
-    sha256 "859ac30a3da796a4ffc744e5018a73f154c4b8dfea71cb21040d21def0aca001"
-  end
+    release_version = "1.9.2"
+    checksums = {
+        "darwin_arm64" => "d1343529dddda3f31728b4fede060584d1461ef88d951d894ed7cb82b6713288",
+        "darwin_amd64" => "c2610bcffd99eb072f56c4b4b60ed2debb3afc2b4c7dee79b8db1868bd32cfea",
+        "linux_arm64"  => "a3d2e062bcab5ae226effee906964ff82c397e37c8d4662b3f8d30b435f42bda",
+        "linux_amd64"  => "09648ecad04aeb1cd59a45bda332b4a7c4e304986687391f2855e6ba6c68b85c",
+    }
 
-  def install
-    libexec.install Dir["*"]
-    bin.install_symlink Dir[libexec/"bin/*"]
+    arch = "arm64"
+    platform = "darwin"
+    on_intel do
+        arch = "amd64"
+    end
+    on_linux do
+        platform = "linux"
+    end
 
-    # Install bash completion
-    output = Utils.safe_popen_read(bin/"encore", "completion", "bash")
-    (bash_completion/"encore").write output
+    url "https://d2f391esomvqpi.cloudfront.net/encore-#{release_version}-#{platform}_#{arch}.tar.gz"
+    version release_version
+    sha256 checksums["#{platform}_#{arch}"]
 
-    # Install zsh completion
-    output = Utils.safe_popen_read(bin/"encore", "completion", "zsh")
-    (zsh_completion/"_encore").write output
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink Dir[libexec/"bin/*"]
 
-    # Install fish completion
-    output = Utils.safe_popen_read(bin/"encore", "completion", "fish")
-    (fish_completion/"encore.fish").write output
-  end
+        # Install bash completion
+        output = Utils.safe_popen_read(bin/"encore", "completion", "bash")
+        (bash_completion/"encore").write output
 
-  test do
-    system "#{bin}/encore", "check"
-  end
+        # Install zsh completion
+        output = Utils.safe_popen_read(bin/"encore", "completion", "zsh")
+        (zsh_completion/"_encore").write output
+
+        # Install fish completion
+        output = Utils.safe_popen_read(bin/"encore", "completion", "fish")
+        (fish_completion/"encore.fish").write output
+    end
+
+    test do
+        system "#{bin}/encore", "check"
+    end
 end
