08d7d3fee0eef0cc80dfb15cdbae7404532c0915
Dominic Black <me@jdm.black>
2023-09-20 02:25:30 +0000

    Release Encore nightly-20230920
    This commit bumps the Encore version to nightly-20230920
    
    _This PR was created by the Encore release process automatically._

--- Formula/encore-nightly.rb
+++ Formula/encore-nightly.rb
@@ -4,12 +4,12 @@ class EncoreNightly < Formula
     license "Mozilla Public License, version 2.0"
     head "https://github.com/encoredev/encore.git", branch: "main"
 
-    release_version = "nightly-20230919"
+    release_version = "nightly-20230920"
     checksums = {
-        "darwin_arm64" => "4480d64f23ea700cb1c3d055b6924dfe08e1b0aa48d81dc69f85f619aa305cb2",
-        "darwin_amd64" => "c7709c1e8efeaf9607f1d84b57954a54f65a1813a3a8aeb7c2bfa54335a22c76",
-        "linux_arm64"  => "b25c8d5a80e078c6d7c91ace4a288afa96c820a935d733196d86a35eff2bbe7e",
-        "linux_amd64"  => "a57c91abe560903ec0ca209abc91f19e91806f44c6bd688037984816a48257ca",
+        "darwin_arm64" => "ae0a15ed8bfbe6fa84525271ba4ed94929d4bb58069e64ca224dc9a708f27d8f",
+        "darwin_amd64" => "65e7dc763a591a2065aa055c5bbec97ff9add76778bf8170b8d830de25291924",
+        "linux_arm64"  => "e0165740d634d55075ad69ff04ebc78f1fed46dc65738cc8c69d316aba7c86ba",
+        "linux_amd64"  => "70f9db9745628b6f3985710e67ddd67e4525f111269da7f7f5d3b8305c908a5e",
     }
 
     arch = "arm64"
