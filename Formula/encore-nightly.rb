a6466f1317c16f6f97f331253135b11817c46c85
André Eriksson <eandre@gmail.com>
2023-11-15 02:24:59 +0000

    Release Encore nightly-20231115
    This commit bumps the Encore version to nightly-20231115
    
    _This PR was created by the Encore release process automatically._

--- Formula/encore-nightly.rb
+++ Formula/encore-nightly.rb
@@ -4,12 +4,12 @@ class EncoreNightly < Formula
     license "Mozilla Public License, version 2.0"
     head "https://github.com/encoredev/encore.git", branch: "main"
 
-    release_version = "nightly-20231114"
+    release_version = "nightly-20231115"
     checksums = {
-        "darwin_arm64" => "293c7d77cd81c4f9f4d3d066d0f8d01de42a3375401bd1aabbf9d0de938e5fca",
-        "darwin_amd64" => "0b1d17459568b6f60bb46b60746f439d5c0cecb47abe633e23e45a5b597ef59f",
-        "linux_arm64"  => "1ffcc40ce053bdd69e7825689d7d16de3e3a9a77b48c5ba5ec7a6fa415d8ab1b",
-        "linux_amd64"  => "907a897a7e60f8197835a283c298f8e1d849ac44ed800843411bb05b564c4331",
+        "darwin_arm64" => "e8efa36abcd84e5d6f0185faa913f11c9d7886481028e374eb2eba0aefa8b3e8",
+        "darwin_amd64" => "c1319c0f0f352d76262d0a210de37529d5cc16987ea08ae754c00ca778f5c14b",
+        "linux_arm64"  => "271187b321a6c3e7c3eafaf053f301d23f528969bcf04fdacde9ac01624f4f87",
+        "linux_amd64"  => "d4748efc5c0af90de0891f602610d579157b37aa0df6f1047f3d882bebb22fd4",
     }
 
     arch = "arm64"
