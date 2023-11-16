e09ef346ccd831619a5035b81cf4c93ec4c50b07
André Eriksson <eandre@gmail.com>
2023-11-16 10:24:00 +0000

    Release Encore 1.25.0-nightly.20231115
    This commit bumps the Encore version to 1.25.0-nightly.20231115
    
    _This PR was created by the Encore release process automatically._

--- Formula/encore-nightly.rb
+++ Formula/encore-nightly.rb
@@ -4,12 +4,12 @@ class EncoreNightly < Formula
     license "Mozilla Public License, version 2.0"
     head "https://github.com/encoredev/encore.git", branch: "main"
 
-    release_version = "nightly-20231115"
+    release_version = "1.25.0-nightly.20231115"
     checksums = {
-        "darwin_arm64" => "e8efa36abcd84e5d6f0185faa913f11c9d7886481028e374eb2eba0aefa8b3e8",
-        "darwin_amd64" => "c1319c0f0f352d76262d0a210de37529d5cc16987ea08ae754c00ca778f5c14b",
-        "linux_arm64"  => "271187b321a6c3e7c3eafaf053f301d23f528969bcf04fdacde9ac01624f4f87",
-        "linux_amd64"  => "d4748efc5c0af90de0891f602610d579157b37aa0df6f1047f3d882bebb22fd4",
+        "darwin_arm64" => "c117b3d19c8058605815303fd38a8b03321b4e63288c7fab132ad0d87f4261b4",
+        "darwin_amd64" => "7b680584bd4c09331f2445a9b92003d447cb2d22db3d6cc7b1ba3319661d3b7a",
+        "linux_arm64"  => "b0b41de5e3fa32fe4e9cb1e108fa866ae8cff519156cead14f3b37158f7aabb9",
+        "linux_amd64"  => "82a6d75e7f01f618721f474e0f968e289a022645c74f520cbb1f74f866d6562b",
     }
 
     arch = "arm64"
@@ -27,22 +27,24 @@ class EncoreNightly < Formula
 
     def install
         libexec.install Dir["*"]
-        bin.install_symlink Dir[libexec/"bin/*"]
+
+        bin.install_symlink libexec/"bin/encore-nightly"
+
 
         # Install bash completion
-        output = Utils.safe_popen_read(bin/"encore", "completion", "bash")
-        (bash_completion/"encore").write output
+        output = Utils.safe_popen_read(bin/"encore-nightly", "completion", "bash")
+        (bash_completion/"encore-nightly").write output
 
         # Install zsh completion
-        output = Utils.safe_popen_read(bin/"encore", "completion", "zsh")
-        (zsh_completion/"_encore").write output
+        output = Utils.safe_popen_read(bin/"encore-nightly", "completion", "zsh")
+        (zsh_completion/"_encore-nightly").write output
 
         # Install fish completion
-        output = Utils.safe_popen_read(bin/"encore", "completion", "fish")
-        (fish_completion/"encore.fish").write output
+        output = Utils.safe_popen_read(bin/"encore-nightly", "completion", "fish")
+        (fish_completion/"encore-nightly.fish").write output
     end
 
     test do
-        system "#{bin}/encore", "check"
+        system "#{bin}/encore-nightly", "check"
     end
 end
