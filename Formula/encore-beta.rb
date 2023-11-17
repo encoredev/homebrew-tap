72c2ced25659ac3cb17ed007b05f2eeec96c94bc
André Eriksson <eandre@gmail.com>
2023-11-17 09:46:36 +0000

    Release Encore 1.29.0-beta.4
    This commit bumps the Encore version to 1.29.0-beta.4
    
    _This PR was created by the Encore release process automatically._

--- Formula/encore-beta.rb
+++ Formula/encore-beta.rb
@@ -4,12 +4,12 @@ class EncoreBeta < Formula
     license "Mozilla Public License, version 2.0"
     head "https://github.com/encoredev/encore.git", branch: "main"
 
-    release_version = "1.29.0-beta.3"
+    release_version = "1.29.0-beta.4"
     checksums = {
-        "darwin_arm64" => "b7564ae7413fd015fab80f62ca66d6f7d421d90cf54f83a8b9227b318ed47c13",
-        "darwin_amd64" => "30af01272514f706556441381ccf44039ede32800bd346cf342e89d3e5cb1cf7",
-        "linux_arm64"  => "949b9caabb564e4cd7f57bc508915ded5ccb9347a2ea8d215587a8a20616e31b",
-        "linux_amd64"  => "abf651a87f107a01a0d39f79745498bcd4ad95dcd266f247044156e7af9041f8",
+        "darwin_arm64" => "fbab5c20cb5318b9aa73936de7640029473419eb01b6614295816672003382a6",
+        "darwin_amd64" => "99a8124540dc489d34b26ba81ead4039007d61e6f67c69f9089b3b52c9621f65",
+        "linux_arm64"  => "5f06abdff547bb7277abf71b25c28633636e41d003571e86bf1acac7908fcaf3",
+        "linux_amd64"  => "de093a8b200f7d6a0ca2af57862cf9ae7b6158e9fefabfd711647989d6b69eae",
     }
 
     arch = "arm64"
