4becebfeb5baa537fa22a9be0b53ed98db5459a0
André Eriksson <eandre@gmail.com>
2026-03-11 10:40:19 +0100

    Release Encore v1.55.0
    This commit bumps the Encore version to v1.55.0
    
    _This commit was created by the Encore release process automatically._

--- Formula/encore.rb
+++ Formula/encore.rb
@@ -4,12 +4,12 @@ class Encore < Formula
     license "Mozilla Public License, version 2.0"
     head "https://github.com/encoredev/encore.git", branch: "main"
 
-    release_version = "1.54.2"
+    release_version = "1.55.0"
     checksums = {
-        "darwin_arm64" => "4a2838d50aa0673d94a9362030c7ef6488395d8727f2430cfdb353c448b87324",
-        "darwin_amd64" => "39d0868c94a247cb6bd0d3e35c0b1eb02a571e4bb36683ce084f56556d161e35",
-        "linux_arm64"  => "31937bc9ce12d3b131015f8d325904cac18a34d92049ceea7526ddb2f919521b",
-        "linux_amd64"  => "4e6ec25f75b3eca49ac527bcef10271cfec3a2159b395cea738b7706bd4dc6ba",
+        "darwin_arm64" => "d6ee428f63bdfaac930fe14a9fab835edf4d7ef7fc55a4251149a8a276331867",
+        "darwin_amd64" => "22af189bf29411621b38b20d51c475ea26b646e20d98db960013c875aec5d624",
+        "linux_arm64"  => "c306410c4ce0c697fc2790ba7cf98353b59fc2999a534d5138883ed2518b615c",
+        "linux_amd64"  => "2cfdedd3c339dc834a80de0ea3e4f090852dacc537951a51eb9936da39e5bf36",
     }
 
     arch = "arm64"
